a73478f6b2bf6bba1970bb3face3b8c6f774fc1b
Ashish <khatriaashish1@gmail.com>
2024-05-03 17:05:59 +0545

    Navbar and hero section built

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./navbar.scss"
+import { motion } from "framer-motion";
 import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa'
+import Sidebar from '../sidebar/Sidebar';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <Sidebar/>
         <div className="wrapper">
-            <span>Ashish Khatri</span>
+            <motion.span 
+            initial={{opacity: 0, scale: 0.5}}
+            animate={{opacity: 1, scale: 1}} transition={{duration: 0.5}}>Ashish Khatri</motion.span>
             <div className="social">
                 <a target='_blank' href="https://github.com/Khatriaashish"><FaGithub/></a>
                 <a target='_blank' href="https://www.linkedin.com/in/ashish-khatri-00504b18b/"><FaLinkedin/></a>
